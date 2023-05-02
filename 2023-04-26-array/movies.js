17ba9c80b13fd85d0e6b19a085700561495d3efe
RomanasV <romanasvenckus@gmail.com>
2023-05-02 11:36:55 +0300

    ciklas cikle

--- 2023-04-26-array/movies.js
+++ 2023-04-26-array/movies.js
@@ -16,7 +16,7 @@ function listMovies(movies) {
   }
 }
 
-listMovies(moviesArr);
+// listMovies(moviesArr);
 
 // 12.2. Sukurti dar 2 naujus masyvus, kuriuose būtų po keletą filmų ir:
 let moviesArr2 = ["Vaikystė", "Jausmų galia", "Gyvenimo medis"];
@@ -24,16 +24,75 @@ let moviesArr3 = ["Ji ji", "Zodiakas ", "Žmonių vaikai", "Žudymo aktas"];
 
 // 12.3. Išvesti visuose 3 masyvuose esančius filmus į ekraną kaip vieną sąrašą.
 
-function listAllMovies(arr1, arr2, arr3) {
-  console.log(arr1);
-  console.log(arr2);
-  console.log(arr3);
+function listAllMovies(movies) {
+  let ulElement = document.querySelector('#movies-1');
+  let count = 1;
 
-  console.log([arr1, arr2, arr3]);
+  // movies1.map((movie) => {
+  //   let movieElement = `<li>${count}. ${movie}</li>`;
+  //   ulElement.innerHTML += movieElement;
+  //   count++;
+  // })
 
-  console.log([...arr1, ...arr2, ...arr3]);
-  console.log(arr1.concat(arr2, arr3));
-  console.log([arr1, arr2, arr3].flat());
+  // movies2.map((movie) => {
+  //   let movieElement = `<li>${count}. ${movie}</li>`;
+  //   ulElement.innerHTML += movieElement;
+  //   count++;
+  // })
+
+  // movies3.map((movie) => {
+  //   let movieElement = `<li>${count}. ${movie}</li>`;
+  //   ulElement.innerHTML += movieElement;
+  //   count++;
+  // })
+
+  // movies.map((innerMovies) => {
+  //   innerMovies.map((movie) => {
+  //     let movieElement = `<li>${count}. ${movie}</li>`;
+  //     ulElement.innerHTML += movieElement;
+  //     count++;
+  //   });
+  // })
+
+  // console.log(movies);
+  // console.log(movies[0]);
+  // console.log(movies[0][0]);
+  // console.log(movies[0][1]);
+  // console.log(movies[0][2]);
+
+  // console.log(movies[1]);
+  // console.log(movies[1][0]);
+  // console.log(movies[1][1]);
+  // console.log(movies[1][2]);
+
+  // console.log(movies[2]);
+  // console.log(movies[2][0]);
+  // console.log(movies[2][1]);
+  // console.log(movies[2][2]);
+  // console.log(movies[2][3]);
+
+
+  // for (let i = 0; i < movies.length; i++) {
+  //   console.log(movies[i]);
+
+  //   for (let j = 0; j < movies[i].length; j++) {
+  //     let movieElement = `<li>${count}. ${movies[i][j]}</li>`;
+  //     ulElement.innerHTML += movieElement;
+  //     count++;
+  //   }
+  // }
+  
+  for (let i = 0; i < movies.length; i++) {
+    let innerMovies = movies[i];
+
+    for (let j = 0; j < innerMovies.length; j++) {
+      let movieElement = `<li>${count}. ${innerMovies[j]}</li>`;
+      ulElement.innerHTML += movieElement;
+      count++;
+    }
+  }
 }
 
-listAllMovies(moviesArr, moviesArr2, moviesArr3);
+let allMoviesArr = [moviesArr, moviesArr2, moviesArr3];
+
+listAllMovies(allMoviesArr);
