ef29a7eb9eeaae11ab296ad584a1e46efda8318d
RomanasV <romanasvenckus@gmail.com>
2023-05-03 13:25:50 +0300

    first cities tasks

--- 2023-05-02-objects/cities.js
+++ 2023-05-02-objects/cities.js
@@ -7,16 +7,131 @@
 //  1.4. touristAttractions - lankytinos vietos (array)
 //  1.5. isCapital - reikšmė nurodanti ar miestas yra sostinė (boolean)
 
-let cities = [
+let citiesData = [
   {
-    name: 'Vilnius',
-    population: 500000,
-
+      name: 'Vilnius',
+      population: 500000,
+      location: {
+          continent: 'Europe',
+          country: 'Lietuva',
+      },
+      touristAttractions: ['Gedimino pilies bokstas', 'Vilniaus katedra'],
+      isCapital: true,
   },
   {
-    name: 'Kaunas',
+      name: 'New York',
+      population: 8500000,
+      location: {
+          continent: 'North America',
+          country: 'United States',
+      },
+      touristAttractions: ['Central park', 'Brooklyn bridge'],
+      isCapital: false,
   },
   {
-
-  }
+      name: 'Tokyo',
+      population: 14000000,
+      location: {
+          continent: 'Asia',
+          country: 'Japan',
+      },
+      touristAttractions: ['Sensō-ji', 'Tokyo Sky Tree'],
+      isCapital: true,
+  },
+  {
+      name: 'Amsterdam',
+      population: 1400000,
+      location: {
+          continent: 'Europe',
+          country: 'Netherlands',
+      },
+      touristAttractions: ['Van gogh museum', 'Rijksmuseum', 'Anne Frank museum'],
+      isCapital: true,
+  },
+  {
+      name: 'Monaco',
+      population: 40000,
+      location: {
+          continent: 'Europe',
+          country: 'Monaco',
+      },
+      touristAttractions: ['Oceanographic museum', "Prince's Palace of Monaco", 'Monaco Grand Prix'],
+      isCapital: true,
+  },
+  {
+      name: 'Havana',
+      population: 2400000,
+      location: {
+          continent: 'North America',
+          country: 'Cuba',
+      },
+      touristAttractions: ['National Capitol of Cuba', 'Plaza de la Catedral'],
+      isCapital: true,
+  },
+  {
+      name: 'Singapore',
+      population: 5600000,
+      location: {
+          continent: 'Asia',
+          country: 'Singapore',
+      },
+      touristAttractions: ['Marina Bay Sands', 'Gardens by the Bay', 'Singapore Zoo'],
+      isCapital: true,
+  },
+  {
+      name: 'Melbourne',
+      population: 5000000,
+      location: {
+          continent: 'Australia',
+          country: 'Australia',
+      },
+      touristAttractions: ['Melbourne Skydeck', 'Queen Victoria Market'],
+      isCapital: false,
+  },
+  {
+      name: 'Sapporo',
+      population: 1900000,
+      location: {
+          continent: 'Asia',
+          country: 'Japan',
+      },
+      touristAttractions: ['Hokkaido Jingu', 'Sapporo Beer Museum'],
+      isCapital: false,
+  },
+  {
+      name: 'Miami',
+      population: 400000,
+      location: {
+          continent: 'North America',
+          country: 'United States',
+      },
+      touristAttractions: ['Miami Zoo', 'Ocean drive', 'Little Havana', 'Vizcaya Museum & Gardens'],
+      isCapital: false,
+  },
 ];
+
+function renderCities(cities) {
+  cities.forEach(city => {
+    // 1.6. Visus miestų masyvų narius išvesti į konsolę.
+    console.log(city);
+    // 1.6.1. Visų miestų pavadinimis išvesti į konsolę.
+    console.log(city.name);
+    // 1.6.2. Visų miestų populiaciją išvesti į konsolę.
+    console.log(city.population);
+    // 1.6.3. Visų miestų žemyną išvesti į konsolę.
+    console.log(city.location.continent);
+    // 1.6.4. Visų miestų šalį išvesti į konsolę.
+    console.log(city.location.country);
+    // 1.6.5. Į konsolę išvesti ar miestas yra sostinė, ar ne.
+    console.log(city.isCapital);
+    // 1.6.6. Į konsolę išvesti visas miesto lankytinas vietas.
+    console.log(city.touristAttractions);
+
+    city.touristAttractions.forEach(attraction => {
+      console.log(attraction);
+    });
+  })
+}
+
+renderCities(citiesData);
+
