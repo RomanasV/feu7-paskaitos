f294959c9d5c110879d1377af69b75f110da4d93
RomanasV <romanasvenckus@gmail.com>
2023-05-08 11:28:45 +0300

    new tasks

--- 2023-05-08-event-listeners/counter.js
+++ 2023-05-08-event-listeners/counter.js
@@ -11,6 +11,8 @@ let plusButton = document.createElement('button');
 // 3. h3 elemento tekstas turėtų būti „5"
 // 4. Mygtukų tekstas turėtų būti „+" ir „-"
 numberDisplay.textContent = 5;
+checkColor();
+
 minusButton.textContent = '-';
 plusButton.textContent = '+';
 
@@ -23,10 +25,42 @@ numbersWrapper.append(numberDisplay, minusButton, plusButton);
 // 5. Sukurti „click" (paspaudimo) event listener'ius abiems mygtukams.
 // 6. „-" mygtuko event listeneris turėtų iškviesti funkciją, kuri sumažina skaičių h3 elemente, o „+" mygtuko paspaudimas turėtų skaičių padidinti
 
+// 7. Jeigu skaitmuo h3 komponente yra mažesnis už du, tai „-" mygtukas turėtų patapti neveiksnus (disabled)- element.setAttribute('disabled', true) / element.removeAttribute('disabled')
+// 8. Jeigu skaitmuo h3 komponente yra 10, tai „+" mygtukas turėtų patapti neveiksnus (disabled)
+
 minusButton.addEventListener('click', () => {
   numberDisplay.textContent = numberDisplay.textContent - 1;
+
+  if (numberDisplay.textContent <= 1) {
+    minusButton.setAttribute('disabled', true);
+  }
+
+  if (numberDisplay.textContent < 10) {
+    plusButton.removeAttribute('disabled');
+  }
+
+  checkColor();
 })
 
 plusButton.addEventListener('click', () => {
   numberDisplay.textContent = Number(numberDisplay.textContent) + 1;
+
+  if (numberDisplay.textContent >= 10) {
+    plusButton.setAttribute('disabled', true);
+  }
+
+  if (numberDisplay.textContent > 1) {
+    minusButton.removeAttribute('disabled');
+  }
+
+  checkColor();
 })
+
+// 9. Jeigu skaitmuo yra 5 arba daugiau, tai jo spalva turėtų būti žalia. Kitu atveju - raudona.
+function checkColor() {
+  if (numberDisplay.textContent < 5) {
+    numberDisplay.style.color = 'red';
+  } else {
+    numberDisplay.style.color = 'green'
+  }
+}
