9cd23f3cfc77db0402f2981c57d12592e3628c55
RomanasV <romanasvenckus@gmail.com>
2023-05-09 14:26:44 +0300

    render student

--- 2023-05-09-forms/script.js
+++ 2023-05-09-forms/script.js
@@ -16,8 +16,51 @@ studentForm.addEventListener('submit', (event) => {
 
   // const studentName = event.target.elements.name.value;
 
-  const studentName = event.target.name.value;
-  
-  console.dir(studentName);
+  const form = event.target;
 
+  const name = form.name.value;
+  const surname = form.surname.value;
+  const age = form.age.value;
+  const phone = form.phone.value;
+  const email = form.email.value;
+  const itKnowledge = form['it-knowledge'].value;
+  const group = form.group.value;
+  
+  console.dir(name);
+  console.dir(surname);
+  console.dir(age);
+  console.dir(phone);
+  console.dir(email);
+  console.dir(itKnowledge);
+  console.dir(group);
+
+  const studentsList = document.querySelector('#students-list');
+  
+  const studentItem = document.createElement('div');
+  studentItem.classList.add('student-item');
+
+  studentsList.prepend(studentItem);
+
+  const nameElement = document.createElement('p');
+  nameElement.innerHTML = `<strong>Name:</strong> ${name}`;
+
+  const surnameElement = document.createElement('p');
+  surnameElement.innerHTML = `<strong>Surname:</strong> ${surname}`;
+
+  const ageElement = document.createElement('p');
+  ageElement.innerHTML = `<strong>Age:</strong> ${age}`;
+
+  const phoneElement = document.createElement('p');
+  phoneElement.innerHTML = `<strong>Phone:</strong> ${phone}`;
+
+  const emailElement = document.createElement('p');
+  emailElement.innerHTML = `<strong>Email:</strong> ${email}`;
+
+  const itKnowledgeElement = document.createElement('p');
+  itKnowledgeElement.innerHTML = `<strong>IT knowledge:</strong> ${itKnowledge}`;
+
+  const groupElement = document.createElement('p');
+  groupElement.innerHTML = `<strong>Group:</strong> ${group}`;
+
+  studentItem.append(nameElement, surnameElement, ageElement, phoneElement, emailElement, itKnowledgeElement, groupElement);
 })
