1f469ac4d39f371a1b46fad805c7857245336bc2
RomanasV <romanasvenckus@gmail.com>
2023-05-17 13:12:12 +0300

    categories

--- 2023-05-17-api/script.js
+++ 2023-05-17-api/script.js
@@ -14,3 +14,16 @@ randomJokeButton.addEventListener('click', () => {
       jokeParagraph.textContent = jokeData.value;
     })
 })
+
+
+fetch('https://api.chucknorris.io/jokes/categories')
+  .then(response => response.json())
+  .then(categories => {
+    const categorySelectElement = document.querySelector('#category-select');
+
+    categories.forEach(category => {
+      const categoryOptionElement = document.createElement('option');
+      categoryOptionElement.textContent = category;
+      categorySelectElement.append(categoryOptionElement);
+    });
+  })
