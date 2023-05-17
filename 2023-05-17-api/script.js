e4581cd14549d4b7ff638471eef302297d4165f5
RomanasV <romanasvenckus@gmail.com>
2023-05-17 14:01:50 +0300

    category select task

--- 2023-05-17-api/script.js
+++ 2023-05-17-api/script.js
@@ -23,7 +23,24 @@ fetch('https://api.chucknorris.io/jokes/categories')
 
     categories.forEach(category => {
       const categoryOptionElement = document.createElement('option');
-      categoryOptionElement.textContent = category;
+      categoryOptionElement.textContent = '- ' + category;
+      categoryOptionElement.value = category;
       categorySelectElement.append(categoryOptionElement);
     });
+
+    const categorySubmitButton = document.querySelector('#category-select-button');
+    categorySubmitButton.removeAttribute('disabled');
   })
+
+const categoryForm = document.querySelector('#category-form');
+
+categoryForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const category = event.target['category-select'].value;
+
+  fetch('https://api.chucknorris.io/jokes/random?category=' + category)
+    .then(response => response.json())
+    .then(jokeData => {
+      jokeParagraph.textContent = jokeData.value;
+    })
+})
