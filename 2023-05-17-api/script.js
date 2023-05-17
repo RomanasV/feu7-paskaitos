e2f3e3ce5a636f6202e53f4b1f32b914b0554a78
RomanasV <romanasvenckus@gmail.com>
2023-05-17 11:44:47 +0300

    chuck norris api jokes by category

--- 2023-05-17-api/script.js
+++ 2023-05-17-api/script.js
@@ -1,7 +1,16 @@
+const randomJokeButton = document.querySelector('#random-joke-button');
 const jokeParagraph = document.querySelector('#joke-paragraph');
 
-fetch('https://api.chucknorris.io/jokes/random')
-  .then(response => response.json())
-  .then(jokeData => {
-    jokeParagraph.textContent = jokeData.value;
-  })
+// fetch('https://api.chucknorris.io/jokes/random')
+//   .then(response => response.json())
+//   .then(jokeData => {
+//     jokeParagraph.textContent = jokeData.value;
+//   })
+
+randomJokeButton.addEventListener('click', () => {
+  fetch('https://api.chucknorris.io/jokes/random')
+    .then(response => response.json())
+    .then(jokeData => {
+      jokeParagraph.textContent = jokeData.value;
+    })
+})
