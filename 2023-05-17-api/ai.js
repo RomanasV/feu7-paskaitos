7357d0d04cdcb06ca535044db24a55041d2d1ba0
RomanasV <romanasvenckus@gmail.com>
2023-05-19 10:04:41 +0300

    async await refactor

--- 2023-05-17-api/ai.js
+++ 2023-05-17-api/ai.js
@@ -7,33 +7,57 @@ function init() {
     const nameInputValue = event.target['name-input'].value;
     nameOutput.textContent = '';
 
-    const ageResponse = await fetch(`https://api.agify.io?name=${nameInputValue}`);
-    const ageData = await ageResponse.json()
-
-    const genderResponse = await fetch(`https://api.genderize.io?name=${nameInputValue}`);
-    const genderData = await genderResponse.json();
-
-    const nationalResponse = await fetch(`https://api.nationalize.io?name=${nameInputValue}`);
-    const nationalData = await nationalResponse.json();
-    const countryCode = nationalData.country[0].country_id;
-
-    const countryResponse = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
-    const countryData = await countryResponse.json();
-
-    const age = ageData.age;
-    const ageOutput = `1. ${nameInputValue} is ${age} years old.`;
-
-    const countryName = countryData[0].name.common;
-    const countryOutput = `2. ${nameInputValue} is from ${countryName}.`;
-    
-    const gender = genderData.gender;
-    const genderOutput = `3. ${nameInputValue} is a ${gender}.`;
+    const ageOutput = await getAgeByName(nameInputValue);
+    const countryOutput = await getCountryByName(nameInputValue);
+    const genderOutput = await getGenderByName(nameInputValue);
 
     nameOutput.textContent = `${ageOutput} ${countryOutput} ${genderOutput}`;
   })
 }
 
+async function getCountryByName(name) {
+  const nationalResponse = await fetch(`https://api.nationalize.io?name=${name}`);
+  const nationalData = await nationalResponse.json();
+  const countryCode = nationalData.country[0].country_id;
+
+  const countryResponse = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+  const countryData = await countryResponse.json();
+
+  const countryName = countryData[0].name.common;
+  const output = `2. ${name} is from ${countryName}.`;
+  
+  return output;
+}
+
+async function getGenderByName(name) {
+  const response = await fetch(`https://api.genderize.io?name=${name}`);
+  const data = await response.json();
+
+  const gender = data.gender;
+  const output = `3. ${name} is a ${gender}.`;
+
+  return output;
+}
+
+async function getAgeByName(name) {
+  const response = await fetch(`https://api.agify.io?name=${name}`);
+  const data = await response.json();
+
+  const age = data.age;
+  const output = `1. ${name} is ${age} years old.`;
+
+  return output;
+}
+
 init();
+
+
+
+
+
+
+
+
 
 
 function regularFetch() {
