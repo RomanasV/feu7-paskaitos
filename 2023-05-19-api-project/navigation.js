eb694ebd338309a3dd621fb83afd118be08075a5
RomanasV <romanasvenckus@gmail.com>
2023-05-22 14:45:26 +0300

    navigation created

--- 2023-05-19-api-project/navigation.js
+++ 2023-05-19-api-project/navigation.js
@@ -1,8 +1,52 @@
 function init() {
+  const headerElement = document.createElement('header');
   const navigationElement = document.createElement('nav');
-  navigationElement.textContent = 'Labas';
+  navigationElement.classList.add('main-navigation');
+  headerElement.append(navigationElement);
 
-  document.body.prepend(navigationElement);
+  const menuList = document.createElement('ul');
+  menuList.classList.add('menu', 'main-menu');
+
+  navigationElement.append(menuList);
+
+  const menuItems = [
+    {
+      title: 'Home',
+      path: ''
+    },
+    {
+      title: 'Users',
+      path: 'users.html'
+    },
+    {
+      title: 'Posts',
+      path: 'posts.html'
+    },
+    {
+      title: 'Albums',
+      path: 'albums.html'
+    }
+  ];
+
+  menuItems.forEach(item => {
+    let { title, path } = item;
+
+    const menuItem = document.createElement('li');
+    menuList.append(menuItem);
+  
+    const menuLink = document.createElement('a');
+
+    if (location.pathname === '/' + path) {
+      menuLink.classList.add('active');
+    }
+
+    menuItem.append(menuLink);
+  
+    menuLink.textContent = title;
+    menuLink.href = './' + path;
+  })
+
+  document.body.prepend(headerElement);
 }
 
 init();
